1d0c5b62bc88a674f10ae55fc7365d4256eeb264
ariel-003 <arielcou@gmail.com>
2022-08-12 23:54:14 -0300

    feat: rendering the next question

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ import { questionsArray } from "./questions.js"
 import { Quiz } from "./models/Quiz.js"
 import { UI } from "./models/Ul.js"
 
+const renderPage = (quiz, ui) => {
+    ui.showQuestion(quiz.getCurrentQuestion().text)
+    ui.showChoices(quiz.getCurrentQuestion().choices, (currentChoice) => {
+        quiz.valodateAndContinue(currentChoice)
+        renderPage(quiz,ui)
+    })
+}
+
 
 const main = () => {
     const quiz = new Quiz(questionsArray)
     const ui = new UI()
-
-    ui.showQuestion(quiz.getCurrentQuestion().text)
-    ui.showChoices(quiz.getCurrentQuestion().choices, (currentChoice) => {
-        quiz.valodateAndContinue(currentChoice)
-    })
+    renderPage(quiz, ui)
+   
    
 }
 
